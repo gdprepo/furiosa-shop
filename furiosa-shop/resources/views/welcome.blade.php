116c4c323c017e2cdeffb2365b72b2da243f5759
dgabin <depairegabin@macbook-pro.home>
2021-01-28 23:20:41 +0100

    upd slider

--- furiosa-shop/resources/views/welcome.blade.php
+++ furiosa-shop/resources/views/welcome.blade.php
@@ -177,10 +177,7 @@
     <?php $index = 0; ?>
     <div id="portfolio-grid" class="row no-gutter" data-aos="fade-up" data-aos-delay="200">
       @foreach($products as $product)
-      @if ($index < 10)
-      
-      
-       <?php
+      @if ($index < 10) <?php
                         $categories = [];
                         foreach ($product->categories as $category) {
                           // $categories = $category->name;
@@ -321,8 +318,26 @@
 @endsection
 
 @section('script-extra')
-
+<script src="https://cdn.jsdelivr.net/npm/axentix@1.0.0/dist/js/axentix.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 <script>
+  let caroulix = new Axentix.Caroulix('#caroulix');
+
+  $(document).ready(function() {
+
+    // Carousel
+
+    $(".carousel").carousel({
+      interval: false,
+      pause: true,
+      touch: true
+    });
+
+
+  });
+
   $('.round').click(function(e) {
     e.preventDefault();
     e.stopPropagation();
